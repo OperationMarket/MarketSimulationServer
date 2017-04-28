281a7fbf5d024aed09a6e899cdfe51f68c1f9d50
Vuton <VutonQ@users.noreply.github.com>
2017-04-27 21:16:35 -0400

    Connection Log

--- main.cpp
+++ main.cpp
@@ -1,35 +1,43 @@
+#include <sys/types.h>
 #include <sys/select.h>
 #include <sys/socket.h>
 #include <string.h>
 #include <microhttpd.h>
 #include <stdio.h>
-#define PORT 8888
+#include <iostream>
+#include <time.h>
+#define PORT 80
 
 static int answer_to_connection (void *cls, struct MHD_Connection *connection, const char *url, const char *method, const char *version, const char *upload_data, size_t *upload_data_size, void **con_cls) {
 
-  const char *page = "<html><body>Hello, browser!</body></html>";
-  struct MHD_Response *response;
-  int ret;
+        const char *page = "Hello, World!";
+        struct MHD_Response *response;
+        int ret;
 
-  response = MHD_create_response_from_buffer(strlen (page), (void *) page, MHD_RESPMEM_PERSISTENT);
-  ret = MHD_queue_response (connection, MHD_HTTP_OK, response);
-  MHD_destroy_response (response);
+        response = MHD_create_response_from_buffer (strlen (page), (void *) page, MHD_RESPMEM_PERSISTENT);
+        ret = MHD_queue_response (connection, MHD_HTTP_OK, response);
 
-  return ret;
+        time_t rawtime;
+        time(&rawtime);
+        std::cout << "Connection on (UTC) " << ctime (&rawtime);
+
+        MHD_destroy_response (response);
+
+        return ret;
 }
 
 
 int main () {
-  struct MHD_Daemon *daemon;
+        struct MHD_Daemon *daemon;
 
-  daemon = MHD_start_daemon (MHD_USE_SELECT_INTERNALLY, PORT, NULL, NULL, &answer_to_connection, NULL, MHD_OPTION_END);
+        daemon = MHD_start_daemon (MHD_USE_SELECT_INTERNALLY, PORT, NULL, NULL, &answer_to_connection, NULL, MHD_OPTION_END);
 
-  if (NULL == daemon){
-    return 1;
-  }
+        if (NULL == daemon){
+                return 1;
+        }
 
-  (void) getchar ();
+        (void) getchar ();
 
-  MHD_stop_daemon (daemon);
-  return 0;
+        MHD_stop_daemon (daemon);
+        return 0;
 }
