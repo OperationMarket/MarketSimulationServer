f241f05721acb6c08ad6dec4bcde6de5e54ec5f5
Ubuntu <ubuntu@ip-172-31-67-198.ec2.internal>
2017-05-11 01:32:53 +0000

    Pls boost

---
+++ CMakeFiles/MarketSimulationServer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MarketSimulationServer.dir/src/main.cpp.o"
+  "CMakeFiles/MarketSimulationServer.dir/src/util/logger.cpp.o"
+  "MarketSimulationServer.pdb"
+  "MarketSimulationServer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MarketSimulationServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
