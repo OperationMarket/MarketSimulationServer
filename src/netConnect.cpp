84b5978ebca89f2fb77316d8e26a71cff4967177
Vuton <VutonQ@users.noreply.github.com>
2017-05-26 18:46:48 -0400

    read until delimiter

--- src/netConnect.cpp
+++ src/netConnect.cpp
@@ -4,45 +4,74 @@ void session::start() {
 	time_t rawtime;
 	time(&rawtime);
 	
-	data_[max_length]='\0';
-	data_[0]='\0';
+	try{
 
-	std::cout << "Connection From: " << socket_.remote_endpoint().address().to_string() << " on: " << ctime (&rawtime);
+		std::cout << "Connection From: " << socket_.remote_endpoint().address().to_string() << " on: " << ctime (&rawtime);
+	}
+	catch (std::exception& e){
+
+		std::cerr << "Exception: " << e.what() << "\n";
+	}
 	do_read();
 }
 
 void session::do_read() {
-	auto self(shared_from_this());
-	socket_.async_read_some(boost::asio::buffer(data_, max_length-1), [this, self](boost::system::error_code ec, std::size_t length){
+	
+	boost::asio::async_read_until(socket_, b, '\n', boost::bind(&session::handler, shared_from_this(), boost::asio::placeholders::error, boost::asio::placeholders::bytes_transferred));
+}
+
+void session::handler(const boost::system::error_code& error, std::size_t bytes_transferred){
+	
+	if (!error && bytes_transferred < max_length) {
 		
-		data_[length]='\0';
-		
-		if (!ec && length < max_length-1) {
-			std::string message = test(data_);
-			do_write(message.size(), message);
-		}
-		else{
+		std::istream is(&b);
+		std::string line;
+		std::getline(is, line);
+
+		std::string message = test(line);
+		//std::cout << message;
+		do_write(message.size(), message);
+	}
+	else{
+		try{
 
 			socket_.shutdown(boost::asio::ip::tcp::socket::shutdown_both);
-                        socket_.close();
+			socket_.close();
 		}
-	});
+		catch (std::exception& e){
+
+			std::cerr << "Exception: " << e.what() << "\n";
+		}
+	}
+
 }
 
 void session::do_write(std::size_t length, std::string message)
 {
-	if(length < max_length-1){
+	if(length < max_length){
 		auto self(shared_from_this());
 		boost::asio::async_write(socket_, boost::asio::buffer(message, length), [this, self](boost::system::error_code ec, std::size_t /*length*/) {
-			
-			socket_.shutdown(boost::asio::ip::tcp::socket::shutdown_both);
-			socket_.close();
+			try{
+
+				socket_.shutdown(boost::asio::ip::tcp::socket::shutdown_both);
+                        	socket_.close();
+			}
+			catch (std::exception& e){
+
+				std::cerr << "Exception: " << e.what() << "\n";
+			}
 		});
 	}
 	else{
+		try{
+		
+        		socket_.shutdown(boost::asio::ip::tcp::socket::shutdown_both);
+       			socket_.close();
+		}
+		catch (std::exception& e){
 
-		socket_.shutdown(boost::asio::ip::tcp::socket::shutdown_both);
-		socket_.close();
+       			 std::cerr << "Exception: " << e.what() << "\n";
+		}
 	}
 }
 
